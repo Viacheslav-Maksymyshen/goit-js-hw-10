81fdc7fa11537407bc848e9afeb3ac3cc98811c1
Viacheslav-Maksymyshen <slavamax.it@gmail.com>
2022-08-03 12:24:00 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
@@ -8,46 +9,39 @@ let refs = {
   list: document.querySelector('.country-list'),
   info: document.querySelector('.country-info'),
 };
-const FULL_NAME = 'name';
-const CAPITAL = 'capital';
-const POPULATION = 'population';
-const FLAGS = 'flags';
-const LANG = 'languages';
 
-const inportText = () => {
-  let name = refs.input.value.trim();
-  fetchCountries(name);
-};
-
-refs.input.addEventListener('input', debounce(inportText, DEBOUNCE_DELAY));
-
-const fetchCountries = name => {
-  if (name.length > 0) {
-    const url = `https://restcountries.com/v3.1/name/${name}?fields=${FULL_NAME},${CAPITAL},${POPULATION},${FLAGS},${LANG}`;
-    fetch(url)
-      .then(response => response.json())
-
-      .then(data => {
-        if (data.status === 404) {
-          Notiflix.Notify.failure('Oops, there is no country with that name');
-          return;
-        }
-        if (data.length > 10) {
-          Notiflix.Notify.info(
-            'Too many matches found. Please enter a more specific name.'
-          );
-          return;
-        }
-        console.log('🚀 ~ file: index.js ~ line 39 ~ data', data);
-        insertContent(data);
-      })
-
-      .catch(error => {
-        console.log('no', error);
-      });
+const importText = () => {
+  if (refs.input.value === '') {
+    refs.list.innerHTML = '';
+    refs.info.innerHTML = '';
+    return;
   }
-  refs.list.innerHTML = '';
+
+  let name = refs.input.value.trim();
+  fetchCountries(name)
+    .then(data => {
+      if (data.status === 404) {
+        refs.list.innerHTML = '';
+        refs.info.innerHTML = '';
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        return;
+      }
+      if (data.length > 10) {
+        refs.list.innerHTML = '';
+        refs.info.innerHTML = '';
+        Notiflix.Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        return;
+      }
+      insertContent(data);
+    })
+    .catch(error => {
+      console.log('no', error);
+    });
 };
+
+refs.input.addEventListener('input', debounce(importText, DEBOUNCE_DELAY));
 
 const createList = item =>
   `<li class="item">
@@ -55,40 +49,42 @@ const createList = item =>
     ${item.name.official}
   </li>`;
 
-// const createItem = item =>
-//   `<li class="item">
-//     <img src = ${item.flags.svg} alt = ${item.name.common} width = 30>
-//     ${item.name.official}
-//   </li>`;
+const createListInfo = item =>
+  `<p><span style= "font-size: 24px; font-weight: 500;">Capital: </span>${
+    item.capital
+  }</p>
+  <p><span style= "font-size: 24px; font-weight: 500;">Population: </span>${
+    item.population
+  }</p>
+  <p><span style= "font-size: 24px; font-weight: 500;">Languages: </span>${Object.values(
+    item.languages
+  )}</p>`;
 
 const generateContentList = array => {
+  const listStyle = 'font-size: 20px; font-weight: 500';
+  const itemStyle = 'font-size: 30px; font-weight: 700';
+
+  refs.info.setAttribute('style', 'font-size: 20px; font-weight: 400');
+
   refs.list.setAttribute(
     'style',
-    'list-style-type: none; font-size: 20px; font-weight: 500; padding: 0; margin: 0;'
+    `list-style-type: none; padding: 0; margin: 0; ${
+      array.length > 1 ? listStyle : itemStyle
+    }`
   );
   return array?.reduce((acc, item) => acc + createList(item), '');
 };
 
-const generateContentItem = array => {
-  refs.list.setAttribute(
-    'style',
-    'list-style-type: none; font-size: 30px; font-weight: 700; padding: 0; margin: 0;'
-  );
-  return array?.reduce((acc, item) => acc + createList(item), '');
+const generateContentItemInfo = array => {
+  return array?.reduce((acc, item) => acc + createListInfo(item), '');
 };
-const generateContentItemInfo = array => `<p>${array.capital}</p>`;
 
 const insertContent = array => {
-  if (array.length > 1) {
-    const resultList = generateContentList(array);
-    refs.list.innerHTML = resultList;
-    return;
+  const resultList = generateContentList(array);
+  refs.list.innerHTML = resultList;
+  refs.info.innerHTML = '';
+  if (array.length === 1) {
+    const resultItemInfo = generateContentItemInfo(array);
+    refs.info.innerHTML = resultItemInfo;
   }
-
-  const resultItem = generateContentItem(array);
-  refs.list.innerHTML = resultItem;
-  const resultItemInfo = generateContentItemInfo(array);
-  console.log('🚀 ~ file: index.js ~ line 91 ~ array', array.capital);
-
-  refs.info.insertAdjacentHTML('beforeend', resultItemInfo);
 };
